9d72a9c3a5169d8850051f938c50fa6d2a038130
Duong Thanh Tan <duongthanhtan17@.com>
2023-11-09 10:06:34 +0700

    progress-report: Cap nhat Progress_Report.docx

--- src/main.py
+++ src/main.py
@@ -3,58 +3,99 @@ import requests
 import json
 import time
 import tempfile
+import concurrent.futures
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 from PyQt5.QtMultimediaWidgets import QVideoWidget
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QLineEdit, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QLineEdit, QLabel, QTextEdit
 from PyQt5.QtCore import QUrl
+from openai import OpenAI
+from cachetools import cached, TTLCache
 
-api_key = 'MGE0ODY2N2Q0MDQxNDE0OTg1OTJlNDkxZDczMjU0NTMtMTY5NzkwMDc5Nw=='
+api_key = 'OTIxNDA3MDM4OTA2NGVlMmJhYjQ1NmNmNWYwMzlmNmItMTY5Nzg3OTAyMA=='
+client = OpenAI(api_key="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")
 
 app = QApplication([])
-#######################################################################################
-### Thư viện Openai đang trong quá trình lỗi, nên hiện tại code phần đó đã bị xóa!! ###
-#######################################################################################
-# Khai báo biến temp_file ngoài hàm để sử dụng chung
-temp_file = None
 
-# Tạo một lớp kế thừa từ QMainWindow để quản lý các widget và sự kiện của cửa sổ chính
+# Create a cache for product info with a Time-To-Live (TTL) of 5 minutes
+cache = TTLCache(maxsize=100, ttl=300)
+
+@cached(cache)
+def get_product_info(product_name):
+    completion = client.chat.completions.create(
+      model="gpt-3.5-turbo",
+      messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": f"Tell me about the product: {product_name}"}
+      ],
+      max_tokens=50
+    )
+    return completion.choices[0].message.content.strip()
+
+def get_video_url(video_id):
+    check_url = 'https://api.heygen.com/v1/video_status.get'
+    params = {'video_id': video_id,}
+    headers = {
+        'X-Api-Key': api_key,
+        'Content-Type': 'application/json',
+    }
+    timeout = time.time() + 60*5  # 5 minutes from now
+    while True:
+        if time.time() > timeout:
+            print("Timeout while waiting for video to be ready.")
+            return None
+        response = requests.get(check_url, params=params, headers=headers)
+        status_response = response.json()
+        if status_response['code'] == 100 and status_response['data']['status'] == 'completed':
+            return status_response['data']['video_url']
+        time.sleep(10)
+
 class VideoWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        # Khởi tạo các thuộc tính như video_widget, player, button, input_text, label
         self.video_widget = QVideoWidget()
         self.player = QMediaPlayer(None, QMediaPlayer.VideoSurface)
         self.player.setVideoOutput(self.video_widget)
-        self.button = QPushButton('Hiển thị video')
-        self.input_text = QLineEdit() # Thêm widget QLineEdit để nhập văn bản vào
-        self.label = QLabel() # Thêm widget QLabel để hiển thị tiêu đề
-        # Gọi phương thức setup_ui để thiết lập giao diện người dùng
+        self.button = QPushButton('Tạo promt')
+        self.video_button = QPushButton('Tạo video') 
+        self.input_text = QLineEdit()
+        self.label = QLabel()
+        self.output_text = QTextEdit()
         self.setup_ui()
     
     def setup_ui(self):
-        # Tạo một widget trung tâm, một layout dọc, và thêm label, input_text, button và video_widget vào layout
         central_widget = QWidget()
-        central_layout = QVBoxLayout()
-        central_layout.addWidget(self.label) # Thêm widget label vào layout dọc trước widget input_text
-        central_layout.addWidget(self.input_text) # Thêm widget input_text vào layout dọc sau widget label
-        central_layout.addWidget(self.button) # Thêm widget button vào layout dọc sau widget input_text
-        central_layout.addWidget(self.video_widget)
+        central_layout = QHBoxLayout() 
+        left_layout = QVBoxLayout() 
+        right_layout = QVBoxLayout() 
+
+        left_layout.addWidget(self.label)
+        left_layout.addWidget(self.input_text)
+        left_layout.addWidget(self.output_text)
+        left_layout.addWidget(self.button)
+        left_layout.addWidget(self.video_button)
+
+        right_layout.addWidget(self.video_widget)
+
+        central_layout.addLayout(left_layout) 
+        central_layout.addLayout(right_layout) 
+
         central_widget.setLayout(central_layout)
-        # Gán widget trung tâm cho cửa sổ chính
         self.setCentralWidget(central_widget)
-        # Kết nối sự kiện clicked của button với phương thức open_video_window
         self.button.clicked.connect(self.open_video_window)
-        # Thiết lập văn bản cho widget label bằng cách sử dụng phương thức setText()
-        self.label.setText('Nhập nội dung vào đây:') # Thiết lập văn bản cho widget label
+        self.video_button.clicked.connect(self.play_video) 
+        self.label.setText('Nhập nội dung vào đây:')
+        self.showMaximized()
+        
+        # Set the stretch factors for the two columns in the layout
+        central_layout.setStretchFactor(left_layout, 1)
+        central_layout.setStretchFactor(right_layout, 1) 
 
     def open_video_window(self):
-        # Gọi phương thức play_video để tải video từ API và phát video trên video_widget
-        self.play_video()
+        product_info = get_product_info(self.input_text.text())
+        self.output_text.setText(product_info)
+        self.product_info = product_info 
 
-    def play_video(self):
-        global temp_file
-        temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".mp4")
-
+        # Send request to create video and get video_id
         create_url = 'https://api.heygen.com/v1/video.generate'
         headers = {
             'X-Api-Key': api_key,
@@ -66,7 +107,7 @@ class VideoWindow(QMainWindow):
                 {
                     "avatar_id": "Daisy-inskirt-20220818",
                     "avatar_style": "normal",
-                    "input_text": self.input_text.text(), # Sử dụng giá trị của widget input_text để thay thế cho "input_text"
+                    "input_text": self.product_info,
                     "offset": {
                         "x": 0,
                         "y": 0
@@ -79,47 +120,28 @@ class VideoWindow(QMainWindow):
             "test": True,
             "version": "v1alpha"
         }
+        response = requests.post(create_url, headers=headers, data=json.dumps(data))
+        if response.status_code == 200:
+            response_data = response.json()
+            self.video_id = response_data['data']['video_id']  # Save video_id
 
-        # Sử dụng các lệnh try-except để bắt các ngoại lệ có thể xảy ra khi gọi API hoặc tải video
-        try:
-            response = requests.post(create_url, headers=headers, data=json.dumps(data))
-
-            if response.status_code == 200:
-                response_data = response.json()
-                video_id = response_data['data']['video_id']
-
-                video_url = None
-                check_url = 'https://api.heygen.com/v1/video_status.get'
-                params = {
-                    'video_id': video_id,
-                }
-                while video_url is None:
-                    time.sleep(10)
-                    response = requests.get(check_url, params=params, headers=headers)
-                    status_response = response.json()
-                    if status_response['code'] == 100 and status_response['data']['status'] == 'completed':
-                        video_url = status_response['data']['video_url']
-
-                print(f'URL của video: {video_url}')
-
-                # Tải video từ video_url xuống tệp tạm thời
+    def play_video(self):
+        if not hasattr(self, 'video_id'):
+            print("Error: No video to play. Please create a prompt first.")
+            return
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            future = executor.submit(get_video_url, self.video_id)  # Use self.video_id
+            video_url = future.result()
+        if video_url is not None:
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_file:
+                print("Downloading video...")
                 response = requests.get(video_url)
                 temp_file.write(response.content)
-
-                # Thiết lập media cho player bằng cách sử dụng QUrl.fromLocalFile(temp_file.name)
+                print("Playing video...")
                 self.player.setMedia(QMediaContent(QUrl.fromLocalFile(temp_file.name)))
-                # Gọi phương thức play() của player
                 self.player.play()
-            else:
-                print(f'Lỗi khi gọi API: {response.status_code}')
-        except Exception as e:
-            print(f'Lỗi khi tải hoặc phát video: {e}')
 
-# Tạo một đối tượng của lớp VideoWindow và hiển thị nó
 video_window = VideoWindow()
 video_window.show()
 
 sys.exit(app.exec_())
-
-# Đóng tệp tạm thời sau khi chơi xong video
-temp_file.close()
