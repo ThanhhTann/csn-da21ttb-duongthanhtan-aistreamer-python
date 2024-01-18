73f5e8006af4584c259f1f52d9b4bcaf0188e1c7
Duong-Thanh-Tan <duongthanhtan17@gmail.com>
2024-01-18 10:03:02 +0700

    ai_streamer: v1.6

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QLineEdit, QLabel, QTextEdit, QComboBox
 from PyQt5.QtCore import QUrl
 from openai import OpenAI
-from cachetools import cached, TTLCache
+
 
 authorization = "API_KEY"
 
@@ -17,80 +17,79 @@ client = OpenAI(api_key="API_KEY")
 
 app = QApplication([])
 
-cache = TTLCache(maxsize=100, ttl=300)
 
-@cached(cache)
-def get_product_info(product_name):
+
+
+def lay_thong_tin_san_pham(ten_san_pham):
     completion = client.chat.completions.create(
       model="gpt-3.5-turbo",
       messages=[
         {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": f"Translate into Vietnamese the following content: Viết đoạn giới thiệu về sản phẩm {product_name}, đoạn văn cần phải hấp dẫn và logic, giống như lời quảng cáo của một MC (xưng hô là em), đoạn văn chỉ 30 từ."}
+        {"role": "user", "content": f"Translate into Vietnamese the following content: Viết đoạn giới thiệu về sản phẩm {ten_san_pham}, đoạn văn cần phải hấp dẫn và logic, giống như lời quảng cáo của một MC (xưng hô là em), đoạn văn chỉ 30 từ."}
       ],
       max_tokens=500
     )
     return completion.choices[0].message.content.strip()
 
-class VideoWindow(QMainWindow):
+class CuaSoVideo(QMainWindow):
     def __init__(self):
         super().__init__()
         self.video_widget = QVideoWidget()
         self.player = QMediaPlayer(None, QMediaPlayer.VideoSurface)
         self.player.setVideoOutput(self.video_widget)
         self.setWindowTitle("AI Streamer") 
-        self.button = QPushButton('Tạo promt')
-        self.video_button = QPushButton('Tạo video') 
-        self.input_text = QLineEdit()
-        self.label = QLabel()
-        self.output_text = QTextEdit()
-        self.voice_combobox = QComboBox()
-        self.voice_combobox.addItem("Nam")
-        self.voice_combobox.addItem("Nữ")
-        self.voice_combobox.setFixedWidth(100)
-        self.voice_label = QLabel("Chọn voice:")
-        self.setup_ui()
+        self.nut_tao_prompt = QPushButton('Tạo prompt')
+        self.nut_tao_video = QPushButton('Tạo video') 
+        self.nhap_text = QLineEdit()
+        self.nhan_dang_label = QLabel()
+        self.ket_qua_text = QTextEdit()
+        self.combobox_giong_noi = QComboBox()
+        self.combobox_giong_noi.addItem("Nam")
+        self.combobox_giong_noi.addItem("Nữ")
+        self.combobox_giong_noi.setFixedWidth(100)
+        self.label_giong_noi = QLabel("Chọn giọng nói:")
+        self.setup_giao_dien()
     
-    def setup_ui(self):
-        central_widget = QWidget()
-        central_layout = QHBoxLayout() 
-        left_layout = QVBoxLayout() 
-        right_layout = QVBoxLayout() 
+    def setup_giao_dien(self):
+        trung_tam_widget = QWidget()
+        trung_tam_layout = QHBoxLayout() 
+        ben_trai_layout = QVBoxLayout() 
+        ben_phai_layout = QVBoxLayout() 
 
-        voice_layout = QHBoxLayout()
-        voice_layout.addStretch(1)  # Add a stretchable space before the label and combobox
-        voice_layout.addWidget(self.voice_label)
-        voice_layout.addWidget(self.voice_combobox)
+        giong_noi_layout = QHBoxLayout()
+        giong_noi_layout.addStretch(1) 
+        giong_noi_layout.addWidget(self.label_giong_noi)
+        giong_noi_layout.addWidget(self.combobox_giong_noi)
 
-        left_layout.addWidget(self.label)
-        left_layout.addWidget(self.input_text)
-        left_layout.addWidget(self.output_text)
-        left_layout.addLayout(voice_layout)
-        left_layout.addWidget(self.button)
-        left_layout.addWidget(self.video_button)
+        ben_trai_layout.addWidget(self.nhan_dang_label)
+        ben_trai_layout.addWidget(self.nhap_text)
+        ben_trai_layout.addWidget(self.ket_qua_text)
+        ben_trai_layout.addLayout(giong_noi_layout)
+        ben_trai_layout.addWidget(self.nut_tao_prompt)
+        ben_trai_layout.addWidget(self.nut_tao_video)
 
-        right_layout.addWidget(self.video_widget)
+        ben_phai_layout.addWidget(self.video_widget)
 
-        central_layout.addLayout(left_layout) 
-        central_layout.addLayout(right_layout) 
+        trung_tam_layout.addLayout(ben_trai_layout) 
+        trung_tam_layout.addLayout(ben_phai_layout) 
 
-        central_widget.setLayout(central_layout)
-        self.setCentralWidget(central_widget)
-        self.button.clicked.connect(self.create_prompt)
-        self.video_button.clicked.connect(self.create_video_and_play) 
-        self.label.setText('Nhập nội dung vào đây:')
+        trung_tam_widget.setLayout(trung_tam_layout)
+        self.setCentralWidget(trung_tam_widget)
+        self.nut_tao_prompt.clicked.connect(self.tao_prompt)
+        self.nut_tao_video.clicked.connect(self.tao_video_va_phat) 
+        self.nhan_dang_label.setText('Nhập nội dung vào đây:')
         self.showMaximized()
         
-        
-        central_layout.setStretchFactor(left_layout, 1)
-        central_layout.setStretchFactor(right_layout, 1) 
+        trung_tam_layout.setStretchFactor(ben_trai_layout, 1)
+        trung_tam_layout.setStretchFactor(ben_phai_layout, 1) 
 
-    def create_prompt(self):
-        product_info = get_product_info(self.input_text.text())
-        self.output_text.setText(product_info)
-        self.product_info = product_info 
+    def tao_prompt(self):
+        thong_tin_san_pham = lay_thong_tin_san_pham(self.nhap_text.text())
+        self.ket_qua_text.setText(thong_tin_san_pham)
+        self.thong_tin_san_pham = thong_tin_san_pham 
 
-    def create_video(self, product_info):
-        voice_id = "vi-VN-NamMinhNeural" if self.voice_combobox.currentText() == "Nam" else "vi-VN-HoaiMyNeural"
+    def tao_video(self, thong_tin_san_pham):
+        giong_noi_id = "vi-VN-NamMinhNeural" if self.combobox_giong_noi.currentText() == "Nam" else "vi-VN-HoaiMyNeural"
         url = "https://api.d-id.com/talks"
         payload = {
             "script": {
@@ -98,17 +97,17 @@ class VideoWindow(QMainWindow):
                 "subtitles": "false",
                 "provider": {
                     "type": "microsoft",
-                    "voice_id": voice_id
+                    "voice_id": giong_noi_id
                 },
                 "ssml": "false",
-                "input": product_info
+                "input": thong_tin_san_pham
             },
             "config": {
                 "fluent": "false",
                 "pad_audio": "0.0",
                 "stitch": "true"
             },
-            "source_url": "https://create-images-results.d-id.com/DefaultPresenters/santa_f_ai/image.jpeg"
+            "source_url": "https://cdn.leonardo.ai/users/a4a05f79-469d-49e1-96de-f3d2b2376537/generations/ba1f512b-aac3-466f-9188-e663cce981fc/variations/alchemyrefiner_alchemymagic_0_ba1f512b-aac3-466f-9188-e663cce981fc_0.jpg?w=512"
         }
         headers = {
             "accept": "application/json",
@@ -122,7 +121,7 @@ class VideoWindow(QMainWindow):
         print(f"Video_id: {id}")
         return id
 
-    def get_video_url(self, video_id):
+    def lay_video_url(self, video_id):
         url = f"https://api.d-id.com/talks/{video_id}"
         headers = {
             "accept": "application/json",
@@ -142,8 +141,9 @@ class VideoWindow(QMainWindow):
                 print("Đang tiến hành GET lại sau 1 giây!!")
                 time.sleep(1)
 
-    def download_and_play_video(self, video_url, player):
+    def tai_va_phat_video(self, video_url, player):
         with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_file:
+            print(f"Tệp tạm thời được tạo tại: {temp_file.name}")
             print("Đang tải video...")
             response = requests.get(video_url)
             temp_file.write(response.content)
@@ -151,13 +151,13 @@ class VideoWindow(QMainWindow):
             player.setMedia(QMediaContent(QUrl.fromLocalFile(temp_file.name)))
             player.play()
 
-    def create_video_and_play(self):
-        self.create_prompt()
-        video_id = self.create_video(self.product_info)
-        video_url = self.get_video_url(video_id)
-        self.download_and_play_video(video_url, self.player)
+    def tao_video_va_phat(self):
+        self.tao_prompt()
+        video_id = self.tao_video(self.thong_tin_san_pham)
+        video_url = self.lay_video_url(video_id)
+        self.tai_va_phat_video(video_url, self.player)
 
-video_window = VideoWindow()
-video_window.show()
+cua_so_video = CuaSoVideo()
+cua_so_video.show()
 
 sys.exit(app.exec_())
